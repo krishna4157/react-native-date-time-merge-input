c054ddd628896fceda5f6cb0f5653e19a6668f4d
krishna4157 <44963465+krishna4157@users.noreply.github.com>
2021-12-19 13:30:37 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,72 @@
+# react-native-date-time-merge-input
+
+<!-- ![](https://i.imgur.com/YxgvSzg.gif) -->
+
+react-native-date-time-merge-input is a plugin to merge both date and time at ease, thus increasing flexibility to use any where in the component. in this component we can also pass text style in params. 
+
+## Getting started
+
+`$ npm install react-native-date-time-merge-input --save`
+
+`$ yarn add react-native-date-time-merge-input`
+
+To use this dependency in your project, the following packages are needed. 
+1. 'react'
+2. 'react-native'
+
+### Mostly automatic installation
+
+
+## Usage (Example of react-native-date-time-merge-input )
+```javascript
+
+
+
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import DTMergeInput from 'react-native-date-time-merge-input';
+/*
+    npm install react-native-date-time-merge-input
+*/
+
+export default class App extends Component{
+    constructor(props){
+        super();
+    }
+    render(){
+        return (
+       <View>
+           <DTMergeInput getValue ={(e)=> 
+           console.log(e);
+           }/>
+        </View>
+        );
+    }
+}
+
+
+// TODO: What to do with the module?
+DTMergeInput;
+```
+
+# required Props and its datatypes :
+
+required Props and its datatypes :
+
+1.  style  : any text styles
+
+
+
+# Credit goes to : 
+
+1. 'react'
+3. 'react-native'
+
+# Contribution :
+
+if you like my work 😀 u can contribute using
+vpa : krishna.santho08@okaxis
+
+it will be a great support to me ☺.
+
+
---
+++ index.js
@@ -0,0 +1,126 @@
+import * as React from 'react';
+import { TextInput } from 'react-native';
+
+export default function DTMergeInput(props) {
+    const [text, setText] = React.useState('');
+    const [length, setLength] = React.useState('');
+    const [minutes, setInMinutes] = React.useState(false);
+
+    const updateFields = value => {
+        setText(value);
+        setLength(value.length);
+        props.getValue(value);
+    }
+
+    const caseCheck = (e, length) => {
+        var s = e.split("");
+        var st = "0" + e;
+        switch (length) {
+            case 0:
+                updateFields(st);
+                break;
+            case 1:
+                updateFields(st);
+                break;
+            case 3:
+                if (e.includes("0") && s[0] == "0") {
+                    s.splice(0, 1);
+                    let t = s.join("");
+                    let st = t + "/";
+                    updateFields(st);
+                } else {
+                    s.splice(2, 0, "/");
+                    let t = s.join("");
+                    updateFields(t);
+                }
+                break;
+            case 4:
+                s.splice(s.indexOf("/") + 1, 0, "0");
+                st = s.join("");
+                console.log(st);
+                updateFields(st);
+                break;
+            case 5:
+                st = e + "/";
+                updateFields(st);
+                break;
+            case 6:
+                if (s[3] == 0) {
+                    s.splice(s.indexOf("/") + 1, 1);
+                    let t = s.join("");
+                    st = t + "/"
+                    updateFields(st);
+                } else {
+                    s.splice(s.indexOf("/") + 3, 0, "/");
+                    let t = s.join("");
+                    st = t;
+                    updateFields(st);
+                }
+                break;
+            case 10:
+                st = e + " ";
+                updateFields(st);
+                break;
+            case 11:
+                if (s[9] != " ") {
+                    s.splice(10, 0, " ");
+                    let t = s.join("");
+                    st = t;
+                    updateFields(st);
+                }
+                break;
+            case 12:
+                s.splice(s.indexOf(" ") + 1, 0, "0");
+                var t = s.join("");
+                st = t;
+                updateFields(st);
+                setInMinutes(false);
+                break;
+            case 13:
+                st = e + ":";
+                updateFields(st);
+                break;
+            case 14:
+                if (!minutes) {
+                    s.splice(s.indexOf(" ") + 1, 1);
+                    let st = s.join("") + ":";
+                    let t = st;
+                    updateFields(t);
+                    setInMinutes(true);
+                } else {
+                    s.splice(s.indexOf(" ") + 3, 0, ":");
+                    let t = s.join("");
+                    updateFields(t);
+                }
+                break;
+            default:
+                updateFields(e);
+                break;
+        }
+    }
+    console.log("props");
+    console.log(props);
+    return (
+        <TextInput
+            maxLength={16}
+            placeholder={'DD/MM/YYYY HH:mm'}
+            value={text}
+            onChangeText={(e) => {
+                e = e.replace(/[A-Za-z&\#,+()$~%.'"*?<>{}@!^]/g, "");
+                if (e.length > length) {
+                    caseCheck(e, e.length);
+                } else {
+                    updateFields(e);
+                }
+            }}
+
+            onKeyPress={({ nativeEvent }) => {
+                if (nativeEvent.key === 'Backspace') {
+                }
+            }}
+
+            style={{ borderBottomWidth: 1 }} keyboardType={'decimal-pad'} />
+    );
+}
+
+
